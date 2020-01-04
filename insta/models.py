62177c196a5050a7e756af73e57994c6f6fbf801
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-04 20:20:31 +0300

    create an image model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+   image_name = models.CharField(max_length =30)
+   image_image = ImageField(blank=True, manual_crop="")
+   image_caption = models.TextField()
+   image_profile = models.ForeignKey(Location, on_delete=models.CASCADE,)
+   image_likes = models.integer()
+   image_comments = models.TextField()   
+   
+   def __str__(self):
+      return self.image_name
