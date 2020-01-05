a16c7aeb0db14f275a78edcfadec9ade8077d3bd
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-05 12:33:11 +0300

    configure login mechanism

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url, include
-
+from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('insta.urls')),
     path('accounts/', include('registration.backends.simple.urls')),
-   
+    path('login/', LoginView.as_view(template_name= '/'), name='user_login'),
 ]
