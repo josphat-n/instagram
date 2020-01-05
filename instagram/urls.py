33ed02b47878a0f7889331e4c728245746730264
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-05 12:13:10 +0300

    redefine the register url

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url, include
-from django_registration.backends.one_step.views import RegistrationView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('insta.urls')),
-    url(r'^accounts/register/',
-        RegistrationView.as_view(success_url='/profile/'),
-        name='django_registration_register'),
-    url(r'^accounts/', include('django_registration.backends.one_step.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+   
 ]
