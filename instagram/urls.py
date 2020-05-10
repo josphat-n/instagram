a06ba7d928ba15d55cc4264788f6d34e294ab60a
Josephat-n <josphatnjoroge254@gmail.com>
2020-05-10 13:22:22 +0300

    make file changes to accomodate change from django registration 2.5.2 to 3.1

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,7 +6,8 @@ from django.contrib.auth.views import LogoutView, LoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('insta.urls')),
-    path('accounts/', include('registration.backends.simple.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('login/', LoginView.as_view(template_name= '/'), name='user_login'),
     path('logout/', LogoutView.as_view(template_name='/'), name="user_logout"),
 ]
